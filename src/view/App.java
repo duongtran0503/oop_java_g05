f5d09b84cce37db10930a41036f10179043c9854
score <duonght2004.itsgu@gmail.com>
2023-09-30 16:34:18 +0700

    init

---
+++ src/view/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
